1fc4905d6b5b33377d4e33a525f7afa62944bd8a
JakSzatan <47736299+JakSzatan@users.noreply.github.com>
2022-03-17 19:31:22 +0100

    Clean up and finishing touches

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,6 @@ import Carousel from "./components/carusel/carusel";
 import "./customstyle.css"
 import {
     BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
   } from "react-router-dom";
 
 const App = () =>{
@@ -16,45 +13,9 @@ const App = () =>{
     return(
         <Router>
         <div>
-            
-            {/* Baner */}
-
             <Navbar/>
-  
-            
-            
-
-
-
-
-
-        {/* <Switch>
-          <Route path="/Ceny">
-          <div className="container mt-5">
-            <h2>Ceny</h2>
-            Tutaj pojawi się cennik
-              <hr/>
-          </div>
-          </Route>
-          <Route path="/About">
-          <div className="container mt-5">
-            <h2>Info</h2>
-             Info o studiu
-            <hr/>
-          </div>
-          </Route>
-          <Route path="/">
-          <Carousel/>
-          <div className="container mt-5">
-            <h2>News</h2>
-            Jakieś Newsy
-              <hr/>
-          </div>
-          </Route>
-        </Switch> */}
         <img src="https://drive.google.com/uc?export=view&id=1TB1YnmiVEk2RLgMtypdfJii8n-djQv6_"  className="img-fluid w-100" alt="Responsive image"/>
         <div className="container mt-5">
-        
             <div data-aos="fade-up" className="mb-5">
               <h2>Kocia Kawiarnia</h2>
               <p>
@@ -70,11 +31,9 @@ const App = () =>{
                </p>
                 <hr id="Koty"/>
             </div>
-
             <div data-aos="fade-up"  className="mb-5">
               <Carousel/>
             </div>
-
             <hr />
             <div data-aos="fade-up" className="mb-5 d-flex">
             <div className="mr-5">
@@ -84,43 +43,31 @@ const App = () =>{
               Fundatorką fundacji jest Pani Krywko, która na swoim koncie ma wiele osiągnięć związanych z opieką i tresurą nad kotami. 
               </p>
             </div>
-
               <hr />
             <div id="Menu" data-aos="fade-up" className="mb-5 d-flex flex-column justify-content-center">
             <h1 className=" text-secondary mx-auto">
                 Można płacić kartą!
                 </h1>
-                
               <img className="rounded w-100" src="https://drive.google.com/uc?export=view&id=1AEwiulATaF0S69PmXjVZ0XLLiu2kK5y3"/>
             </div>
-
           </div>
-
             <div data-aos="fade" className="pt-2 pl-md-5 pl-sm-2 pl-1 pb-4 bg-secondary">
-              
             <h2 className="pl-md-5 pl-sm-2 pl-1">Skontaktuj sie z nami</h2>
             <div className="pl-md-5 pl-sm-2 pl-1" style={{color:"#5a5a5a", fontSize:"20px"}}>   
-              
                 <div>
                 <span><i  className="bi bi-geo-alt text-primary ml-1 fs-1"></i></span>
                 Świętego Wojciecha 1
                 10-102 Olsztyn
                 <p><span ><i  className="bi bi-envelope ml-2 " style={{color:"#eee"}}> kociakawiarnia.okk@gmail.com</i></span></p>
-
               </div>   
                   <div style={{color:"#fccc63"}}>
                   <span><a href="https://www.facebook.com/KociaKawiarniaOlsztyn/"><i  className="bi bi-facebook ml-2 " style={{fontSize:"30px",color:"#516EE1"}}></i></a></span>
                   <span><a href="https://www.instagram.com/kocia.kawiarnia.olsztyn/" style={{fontSize:"30px"}}><i className="bi bi-instagram instagram ml-2 "></i></a></span>
                   <i className="bi bi-telephone ml-2 fs-2"></i> 607 404 044</div>
-
             </div>
             </div>
       </div>
     </Router>
-
-      
-        
-
     );
 }
 
